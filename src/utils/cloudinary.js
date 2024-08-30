c9c3e61f42435b882c340e2c5e04cea5cb0a301f
nandishbt <nandishbt2001@gmail.com>
2024-08-30 13:23:53 +0530

    files uploading finished

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,50 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+// Configuration
+cloudinary.config({
+  cloud_name: CLOUDINARY_CLOUD_NAME,
+  api_key: CLOUDINARY_API_KEY,
+  api_secret: CLOUDINARY_API_SECRET
+});
+
+async function savetoCloudinary(localFilepath) { //saveing local file to cloudinary
+ try {
+    const res = await cloudinary.uploader.upload(localFilepath,{
+        resource_type: "auto",
+        public_id: "videotube"
+       
+    })
+    console.log(res.url);
+    fs.unlink(localFilepath);  // Delete the local file after upload to Cloudinary
+    return res;
+    
+ } catch (error) {
+     console.error(`Error saving to Cloudinary: ${error.message}`);
+     fs.unlink(localFilepath); // Delete local file path if upload fails
+     return null;
+    
+ }
+}
+
+
+
+  // Optimize delivery by resizing and applying auto-format and auto-quality
+//   const optimizeUrl = cloudinary.url("shoes", {
+//     fetch_format: "auto",
+//     quality: "auto",
+//   });
+
+//   console.log(optimizeUrl);
+
+//   // Transform the image: auto-crop to square aspect_ratio
+//   const autoCropUrl = cloudinary.url("shoes", {
+//     crop: "auto",
+//     gravity: "auto",
+//     width: 500,
+//     height: 500,
+//   });
+
+//   console.log(autoCropUrl);
+
+export {savetoCloudinary}
